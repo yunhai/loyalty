bc76f050609f4bf806a62314128e752e0c62d936
DESKTOP-RI3J40L\ADMIN <haminhman2011@gmail.com>
2019-06-24 21:50:09 +0700

    rmove

--- application/logs/log-2019-05-16.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2019-05-16 22:04:21 --> Severity: error --> Exception: Unable to locate the model you have specified: Mproducts D:\CT\XAMPP\htdocs\freelancer\crm_fb\system\core\Loader.php 344
-ERROR - 2019-05-16 22:08:38 --> Severity: Warning --> json_decode() expects parameter 1 to be string, object given D:\CT\XAMPP\htdocs\freelancer\crm_fb\application\controllers\Cron.php 32
-ERROR - 2019-05-16 17:20:47 --> Severity: Parsing Error --> syntax error, unexpected ')' D:\CT\XAMPP\htdocs\freelancer\crm_fb\application\controllers\Cron.php 31
-ERROR - 2019-05-16 22:21:26 --> Severity: Warning --> curl_setopt() expects exactly 3 parameters, 2 given D:\CT\XAMPP\htdocs\freelancer\crm_fb\application\controllers\Cron.php 51
-ERROR - 2019-05-16 22:22:06 --> Severity: Notice --> Undefined variable: postStr D:\CT\XAMPP\htdocs\freelancer\crm_fb\application\controllers\Cron.php 51
-ERROR - 2019-05-16 22:23:33 --> Severity: Notice --> Array to string conversion D:\CT\XAMPP\htdocs\freelancer\crm_fb\application\controllers\Cron.php 48
-ERROR - 2019-05-16 22:23:52 --> Severity: Notice --> Array to string conversion D:\CT\XAMPP\htdocs\freelancer\crm_fb\application\controllers\Cron.php 48
