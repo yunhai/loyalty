9bf5f94b9a626596d6cb05f3a0d7c2a44f12ed37
pandog <yunhaihuang@gmail.com>
2019-07-14 09:54:11 +0700

    q!

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -6,26 +6,32 @@ class Site extends MY_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->model('Mconfigs');
+        $this->load->library('user_agent');
     }
 
-    private function commonDataSite($title = ''){
-        $configSites =  $this->Mconfigs->getListMap();
-        return array(
-            'title' => $title . $configSites['COMPANY_NAME'],
-            'totalItemCart' => $this->cart->total_items(),
-            'configSites' => $configSites
-        );
-    }
+    // private function commonDataSite($title = ''){
+    //     $configSites =  $this->Mconfigs->getListMap();
+    //     return array(
+    //         'title' => $title . $configSites['COMPANY_NAME'],
+    //         'totalItemCart' => $this->cart->total_items(),
+    //         'configSites' => $configSites
+    //     );
+    // }
 
     public function index(){
         $user = $this->session->userdata('user');
         $num = 0;
         $this->load->model(array('Mcustomersanticipates','Mquestions'));
-        if($user){
+        if ($user) {
             $num = $this->Mcustomersanticipates->getNum($user['UserId']);
         }
         $listQuestions = $this->Mquestions->getBy(array('StatusId' => STATUS_ACTIVED));
-        $this->load->view('site/home', array('user' =>$user, 'num' => $num, 'listQuestions' => $listQuestions));
+        $this->load->view('site/home', array(
+            'user' =>$user, 
+            'num' => $num, 
+            'listQuestions' => $listQuestions,
+            'is_sp' => $this->agent->mobile()
+        ));
     }
 
     public function logout(){
